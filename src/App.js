f52ab5466ecc9003f66ccfcf400c3c5af8fae22a
hari1538radha <hari.r2020ece@sece.ac.in>
2022-12-20 12:51:57 +0530

    fix : code formated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./component/Navbar/Navbar";
 import About from "./component/About/About";
-import Bg from"./../src/component/Images/bg.svg";
+import Bg from "./../src/component/Images/bg.svg";
 function App() {
   return (
-    <div className="bg-cover bg-no-repeat" style={{ backgroundImage: `url(${Bg})`}}>
+    <div
+      className="bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${Bg})` }}
+    >
       <Navbar />
       <About />
     </div>
--- src/component/About/About.js
+++ src/component/About/About.js
@@ -6,7 +6,9 @@ const About = () => {
     <div className="flex justify-between h-[683px] pr-[10%] ">
       <div className="flex flex-col w-2/4 justify-center text-start items-center pl=[10%]">
         <p className="text-[40px] font-medium">Roshan B</p>
-        <p className="text-[30px] font-medium text-[#4FA095]">UI & UX Designer</p>
+        <p className="text-[30px] font-medium text-[#4FA095]">
+          UI & UX Designer
+        </p>
       </div>
       <div className="pt-11">
         <img src={Profile}></img>
